36e568287bead50bb9f1691d31f971d0b3352a60
Joshua White <2314105@leedstrinity.ac.uk>
2024-02-21 11:17:44 +0000

    flash messages error handeling

--- Flask-Web-App-Tutorial/website/auth.py
+++ Flask-Web-App-Tutorial/website/auth.py
@@ -1,11 +1,9 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html", boolean = True)
   
 @auth.route('/logout')
@@ -14,4 +12,29 @@ def logout():
   
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email1 = request.form.get('email1')
+        email2 = request.form.get('email2')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        university = request.form.get('university')
+        
+        if email1 < 4:
+            flash("Email is too short", category="error")
+            pass
+        elif email1 != email2:
+            flash("Emails don't match", category="error")
+            pass
+        elif password1 < 8:
+            flash("Password is too short", category="error")
+            pass
+        elif password1 != password2:
+            flash("Passwords don't match", category="error")
+            pass
+        else:
+            flash("Account created", category="success")
+            # add user to database
+            pass
     return render_template("sign_up.html")
