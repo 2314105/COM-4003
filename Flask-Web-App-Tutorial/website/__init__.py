47e972d9242f070bad1744c215b6e4e6c6229b2d
Joshua White <2314105@leedstrinity.ac.uk>
2024-02-21 15:04:16 +0000

    fixed create database

--- Flask-Web-App-Tutorial/website/__init__.py
+++ Flask-Web-App-Tutorial/website/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -14,7 +15,7 @@ def create_app():
     from .views import views
     from .auth import auth
     
-    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/') 
     app.register_blueprint(auth, url_prefix='/')
     
     from .models import User, Note
@@ -24,6 +25,6 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+    with app.app_context():
+        db.create_all()
+        print('Database created!')
