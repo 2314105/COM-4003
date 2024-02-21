124cbb0209cf1cd1142d59504c2bded5a720bc28
Joshua White <2314105@leedstrinity.ac.uk>
2024-02-21 12:01:55 +0000

    sql setup

--- Flask-Web-App-Tutorial/website/auth.py
+++ Flask-Web-App-Tutorial/website/auth.py
@@ -21,26 +21,26 @@ def sign_up():
         password2 = request.form.get('password2')
         university = request.form.get('university')
         
-        if email1 < 4:
+        if len(email1) < 4:
             flash("Email is too short", category="error")
-            pass
-        elif firstName < 2:
+            return render_template("sign_up.html")
+        elif len(firstName) < 2:
             flash("First name is too short", category="error")
-            pass
-        elif lastName < 2:
+            return render_template("sign_up.html")
+        elif len(lastName) < 2:
             flash("Last name is too short", category="error")
-            pass
+            return render_template("sign_up.html")
         elif email1 != email2:
             flash("Emails don't match", category="error")
-            pass
-        elif password1 < 8:
+            return render_template("sign_up.html")
+        elif len(password1) < 8:
             flash("Password is too short", category="error")
-            pass
+            return render_template("sign_up.html")
         elif password1 != password2:
             flash("Passwords don't match", category="error")
-            pass
+            return render_template("sign_up.html")
         else:
             flash("Account created", category="success")
             # add user to database
-            pass
+            return render_template("sign_up.html")
     return render_template("sign_up.html")
