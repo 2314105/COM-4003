dc44cc2a696c7ddb992cef4dddc28ecec957aa5e
Joshua White <2314105@leedstrinity.ac.uk>
2024-02-22 08:54:42 +0000

    innit changes, database still not insde website folder

--- Flask-Web-App-Tutorial/website/__init__.py
+++ Flask-Web-App-Tutorial/website/__init__.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
-
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' 
     db.init_app(app)
     
     from .views import views
@@ -29,5 +28,4 @@ def create_database(app):
         with app.app_context():
             db.create_all()
             print('Database created!')
-
 
